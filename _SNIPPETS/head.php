82bc21b902ed0e6d4186675e9b4a0469caf7a814
Nicholas Cappo <nc2b6@mail.missouri.edu>
2014-11-07 15:15:04 -0600

    Changed Title
    To Fantasy Footbal

--- _SNIPPETS/head.php
+++ _SNIPPETS/head.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>CS3380 - Lab 8</title>
+	<title>Fantasy Football</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
