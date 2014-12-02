4b5da1de0a53cd7c1da61efa44e450291d47af23
Nicholas Cappo <nc2b6@mail.missouri.edu>
2014-12-01 22:01:46 -0600

    MAYBE?

--- week.php
+++ week.php
@@ -23,14 +23,16 @@
 			// update team set points=points+player[points]
 	$teams = pg_prepare($conn, 'get_teams', "SELECT team_id FROM master.team WHERE league=$1;") or die("Failed to create get teams query");
 	$teams = pg_execute($conn, 'get_teams', array($logged_in)) or die("Failed to execute state update query");
-	while ($team = pg_fetch_array($teams, NULL, PGSQL_ASSOC)){
+	$team = pg_fetch_array($teams, NULL, PGSQL_ASSOC);
+	do{
 		$players = pg_prepare($conn, 'get_players', "SELECT points FROM gamedb.gameweek$1 RIGHT JOIN (SELECT * FROM master.draft WHERE master.draft.team_id=$2) AS Draft ON gamedb.gameweek$1.id=Draft.player_id;") or die("Failed to create get players query".pg_last_error());
 		$players = pg_execute($conn, 'get_players', array($_SESSION['week'],$team['team_id'])) or die("Failed to execute get players query".pg_last_error());
-		while ($player = pg_fetch_array($players, NULL, PGSQL_ASSOC)){
+		$player = pg_fetch_array($players, NULL, PGSQL_ASSOC);
+		do {
 			$points = pg_prepare($conn, 'put_points', "UPDATE master.team SET points=points+$1 WHERE master.team.team_id=$2;") or die("Failed to points get players query".pg_last_error());
 			$points = pg_execute($conn, 'put_points', array($player['points'],$team['team_id'])) or die("Failed to execute points update query".pg_last_error());
-		}
-	}
+		} while ($player = pg_fetch_array($players, NULL, PGSQL_ASSOC));
+	} while ($team = pg_fetch_array($teams, NULL, PGSQL_ASSOC));
 	
 	
 	// Set next week
