fef48530defc82ae64e595c08bc8255254b4d5f7
Nicholas Cappo <nc2b6@mail.missouri.edu>
2014-12-01 23:51:15 -0600

    asfd

--- trade.php
+++ trade.php
@@ -51,7 +51,8 @@
 <div class="container">
 <?php
 echo'<div class="jumbotron"><h1>Trade<br><small>Pick teams below</small></h1>';
-echo'<form method="POST" action="trade.php">
+echo'<form class="form" method="POST" action="trade.php">
+		<div class="form-group">
 			<select class="form-control" name="team1">';
 			//select all teams, display
 			$teams = pg_prepare($conn, 'trade_teams', "SELECT team_id,name FROM master.team WHERE league=$1;") or die("Failed to create draft team query".pg_last_error());
@@ -59,13 +60,14 @@ echo'<form method="POST" action="trade.php">
 			while($team = pg_fetch_array($teams, NULL, PGSQL_ASSOC)){
 			echo'<option value="'.$team['team_id'].'">'.$team['name'].'</option>';
 			}
-echo '		</select>
+echo '		</select></div>
+			<div class="form-group">
 			<select class="form-control" name="team2">';
 			$teams = pg_execute($conn, 'trade_teams', array($logged_in)) or die("Failed to execute draft team query".pg_last_error());
 			while($team = pg_fetch_array($teams, NULL, PGSQL_ASSOC)){
 			echo'<option value="'.$team['team_id'].'">'.$team['name'].'</option>';
 			}
-echo '		</select>
+echo '		</select></div>
 			<input class="btn btn-success" name="submit_teams" type="submit" value="Trade">
 		</form>';
 echo'</div>';
@@ -76,7 +78,7 @@ echo'</div>';
 <?php
 if (isset($_POST['submit_teams'])){
 echo'
-		<form method="POST" action="trade.php">
+		<form class="form" method="POST" action="trade.php">
 			<select class="form-control" name="player1">';
 			$players = pg_prepare($conn, 'trade_players', "SELECT id,lname,fname FROM seasondb.season INNER JOIN (select player_id FROM master.draft WHERE league=$1 AND team_id=$2) AS Draft ON seasondb.season.id=Draft.player_id;") or die("Failed to create draft team query".pg_last_error());
 			$players = pg_execute($conn, 'trade_players', array($logged_in,intval($_POST['team1']))) or die("Failed to execute draft team query".pg_last_error());
